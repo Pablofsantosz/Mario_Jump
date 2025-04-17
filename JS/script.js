3a556c624b7c9da85b1a6b47d86953adf2426fe1
pablo felipe <pabloletiane@gmail.com>
2025-04-16 23:48:27 -0300

    commit

---
+++ JS/script.js
@@ -0,0 +1,12 @@
+const mario = document.querySelector('.mario');
+const jump = ()=>{
+    mario.classList.add('jump'); 
+    /* o set timeout funciona de forma que ele recebe 2 paremetros uma funçao e um tempo settimeout(funcao,tempo) */
+    setTimeout(()=>{
+        mario.classList.remove('jump');
+
+    },500) 
+
+}
+
+document.addEventListener("keydown",jump) 
