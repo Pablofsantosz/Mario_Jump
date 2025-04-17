60ab040f73a2f9654931f7f7c48346c763a9dcbc
pablo felipe <pabloletiane@gmail.com>
2025-04-17 00:15:03 -0300

    commit

--- JS/script.js
+++ JS/script.js
@@ -1,12 +1,18 @@
 const mario = document.querySelector('.mario');
+
 const jump = ()=>{
     mario.classList.add('jump'); 
     /* o set timeout funciona de forma que ele recebe 2 paremetros uma funçao e um tempo settimeout(funcao,tempo) */
-    setTimeout(()=>{
+    setTimeout(()=> {
         mario.classList.remove('jump');
-
-    },500) 
+  
+    },500);    
 
 }
 
-document.addEventListener("keydown",jump) 
+
+document.addEventListener("keydown", (event) => {
+    if (event.code === 'Space') {
+        jump();
+    } 
+}); 
