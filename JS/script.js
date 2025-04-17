42fbfb702784fcd290aff14bbd7dbc30ef98eca1
pablo felipe <pabloletiane@gmail.com>
2025-04-17 00:33:23 -0300

    Tubo stop

--- JS/script.js
+++ JS/script.js
@@ -1,4 +1,5 @@
 const mario = document.querySelector('.mario');
+const pipe = document.querySelector('.pipe');
 
 const jump = ()=>{
     mario.classList.add('jump'); 
@@ -9,6 +10,17 @@ const jump = ()=>{
     },500);    
 
 }
+
+const loop = setInterval(()=>{
+    const pipePosition = pipe.offsetLeft;
+    console.log(pipePosition);
+
+    if(pipePosition <= 120){
+        pipe.style.animation = 'none';
+        pipe.style.left = `${pipePosition}px`;
+    }
+     
+},10);
 
 
 document.addEventListener("keydown", (event) => {
