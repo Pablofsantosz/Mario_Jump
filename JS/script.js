fa24412737bec20279936d0ac320219b2983a6ca
pablo felipe <pabloletiane@gmail.com>
2025-04-30 09:47:41 -0300

    commit correção bug

--- JS/script.js
+++ JS/script.js
@@ -1,27 +1,32 @@
 const mario = document.querySelector('.mario');
 const pipe = document.querySelector('.pipe');
+const clouds = document.querySelector('.clouds');
 const scoreDisplay = document.getElementById('score');
 const restartBtn = document.getElementById('restartBtn');
 
 let score = 0;
+let isGameOver = false;
 
 const jump = () => {
-    mario.classList.add('jump');
-    setTimeout(() => {
-        mario.classList.remove('jump');
-    }, 500);
+    if (!isGameOver) {
+        mario.classList.add('jump');
+        setTimeout(() => {
+            mario.classList.remove('jump');
+        }, 500);
+    }
 };
 
 const loop = setInterval(() => {
     const pipePosition = pipe.offsetLeft;
     const marioPosition = +window.getComputedStyle(mario).bottom.replace('px', '');
-
-    score++;
-    scoreDisplay.textContent = score;
+    const cloudsPosition = +window.getComputedStyle(clouds).right.replace('px', '');
 
     if (pipePosition <= 120 && pipePosition > 0 && marioPosition < 80) {
         pipe.style.animation = 'none';
         pipe.style.left = `${pipePosition}px`;
+
+        clouds.style.animation = 'none';
+        clouds.style.right = `${cloudsPosition}px`;
 
         mario.style.animation = 'none';
         mario.style.bottom = `${marioPosition}px`;
@@ -29,10 +34,18 @@ const loop = setInterval(() => {
         mario.style.width = '75px';
         mario.style.marginLeft = '50px';
 
-        clearInterval(loop);
+        isGameOver = true;
         restartBtn.style.display = 'block';
+        clearInterval(loop);
     }
+
+    if (!isGameOver) {
+        score++;
+        scoreDisplay.textContent = score;
+    }
+
 }, 10);
+
 
 document.addEventListener("keydown", (event) => {
     if (event.code === 'Space') {
